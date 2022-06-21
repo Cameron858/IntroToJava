03ade1e2e4792bc29c6cd362ed96b088d2d31b62
Cameron <cameronwood858@googlemail.com>
2022-06-21 23:18:18 +0100

    Refactored game into seperate class and file. Main.java now runs using this class.

---
+++ src/tutorial1/Main.java
@@ -0,0 +1,16 @@
+package tutorial1;
+
+
+public class Main {
+
+    /** 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        RandomNumberGuesser game = new RandomNumberGuesser(0, 10, 5);
+        game.play();
+        
+        }
+
+}
---
+++ src/tutorial1/RandomNumberGuesser.java
@@ -0,0 +1,71 @@
+package tutorial1;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Implements a simple random number guess game.
+ */
+public class RandomNumberGuesser {
+
+    private int min;
+    private int max;
+    private int lives;
+
+    /**
+     * 
+     * @param min Minimum random number the answer can be.
+     * @param max Maximum random number the answer can be.
+     * @param lives Number of attempts the user has to guess the answer
+     */
+    public RandomNumberGuesser(int min, int max, int lives) {
+        this.min = min;
+        this.max = max;
+        this.lives = lives;
+    }
+
+    /**
+     * Runs the game.
+     */
+    public void play() {
+
+        Scanner sc = new Scanner(System.in);
+        String name = this.GetUsername(sc);
+        int guess;
+        int answer = ThreadLocalRandom.current().nextInt(this.min, this.max + 1); // sets random integer to between min and max values
+        
+        while (true) {
+            System.out.print("Enter your guess:");
+            guess = sc.nextInt();
+
+            if (guess == answer) {
+                System.out.println("Congrats " + name + ", you won!");
+                break;
+            } else if (guess > answer) {
+                System.out.println("Too high!");
+            } else if (guess < answer) {
+                System.out.println("Too low!");
+            } else if (this.lives == 0) {
+                System.out.println("You ran out of lives!");
+                System.out.println("The answer was " + answer);
+                break;
+            }
+
+            this.lives--;
+        }
+
+        sc.close();
+    }
+
+    
+    /** 
+     * @param sc
+     * @return String
+     */
+    private String GetUsername(Scanner sc) {
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        return name;
+    }
+
+}
