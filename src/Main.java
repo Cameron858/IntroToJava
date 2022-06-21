c4d3714e92d802d729ca2a90f21ea49c0835835e
Cameron <cameronwood858@googlemail.com>
2022-06-21 22:10:08 +0100

    GetName and PrintFinalMessage methods created.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter your name: ");
-        String name = sc.nextLine();
 
-        int answer = ThreadLocalRandom.current().nextInt(0, 11);    // sets random integer to 0 -> 10 inclusive
+        String name = GetName(sc);
+        int answer = ThreadLocalRandom.current().nextInt(0, 11); // sets random integer to 0 -> 10 inclusive
         int guess;
         int guesses = 0;
 
@@ -19,12 +17,23 @@ public class Main {
             guess = sc.nextInt();
             guesses++;
         } while (guess != answer);
-        
+
+        PrintFinalMessage(name, guesses, answer);
+
+        sc.close();
+    }
+
+    public static String GetName(Scanner sc) {
+
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        return name;
+    }
+
+    public static void PrintFinalMessage(String name, int guesses, int answer) {
         System.out.println("Well done " + name + "!");
         System.out.println("You had " + guesses + " guesses!");
         System.out.println("The answer was " + answer + "!");
-
-        sc.close();
-
     }
 }
